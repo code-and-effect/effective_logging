385b50ae98cd042e70dd4b69c30e016414338031
Matt Riemer <matthew@agilestyle.com>
2015-03-10 13:44:46 -0600

    Log the user sign in after a password reset

--- lib/effective_logging/user_logger.rb
+++ lib/effective_logging/user_logger.rb
@@ -10,6 +10,18 @@ module EffectiveLogging
         )
       end
 
+      Warden::Manager.after_set_user do |user, warden, opts|
+        if (opts[:event] == :set_user rescue false) # User has just reset their password and signed in
+          EffectiveLogger.success('user login',
+            :user => user,
+            :ip => warden.request.ip.presence,
+            :referrer => warden.request.referrer,
+            :user_agent => warden.request.user_agent,
+            :notes => 'after password reset'
+          )
+        end
+      end
+
       Warden::Manager.before_logout do |user, warden, opts|
         if user.respond_to?(:timedout?) && user.respond_to?(:timeout_in)
           scope = opts[:scope]
