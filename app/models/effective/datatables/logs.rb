bcbbfab84a03cf85dfc46ce92fdf711942c49566
Matt Riemer <matthew@agilestyle.com>
2015-07-24 15:32:00 -0600

    Upgrade effective_datatables

--- app/models/effective/datatables/logs.rb
+++ app/models/effective/datatables/logs.rb
@@ -4,41 +4,42 @@ if defined?(EffectiveDatatables)
       class Logs < Effective::Datatable
         include EffectiveLoggingHelper
 
-        default_order :created_at, :desc
+        datatable do
+          default_order :created_at, :desc
 
-        table_column :created_at
+          table_column :created_at
+          table_column :id, visible: false
 
-        table_column :id, visible: false
+          table_column :parent_id, visible: false
+          table_column :user, :if => proc { attributes[:user_id].blank? }
 
-        table_column :parent_id, visible: false
-        table_column :user, :if => proc { attributes[:user_id].blank? }
+          table_column :status, filter: { type: :select, values: EffectiveLogging.statuses }
 
-        table_column :status, filter: { type: :select, values: EffectiveLogging.statuses }
+          table_column :message, width: '50%', sortable: false
+          table_column :logs_count, visible: false
 
-        table_column :message, width: '50%', sortable: false
-        table_column :logs_count, visible: false
+          table_column :details, visible: false, sortable: false do |log|
+            log.details.delete(:email)
+            tableize_hash(log.details, th: true, sub_th: false, width: '100%')
+          end
 
-        table_column :details, visible: false, sortable: false do |log|
-          log.details.delete(:email)
-          tableize_hash(log.details, th: true, sub_th: false, width: '100%')
-        end
+          table_column :updated_at, visible: false
 
-        table_column :updated_at, visible: false
+          table_column :actions, sortable: false, filter: false do |log|
+            show_path =
+              if datatables_active_admin_path?
+                admin_effective_log_path(log)
+              elsif datatables_admin_path?
+                effective_logging.admin_log_path(log)
+              else
+                effective_logging.log_path(log)
+              end
 
-        table_column :actions, sortable: false, filter: false do |log|
-          show_path =
-            if datatables_active_admin_path?
-              admin_effective_log_path(log)
-            elsif datatables_admin_path?
-              effective_logging.admin_log_path(log)
+            if log.logs_count.to_i > 0
+              link_to "View&nbsp;(#{log.logs_count}&nbsp;more)".html_safe, show_path
             else
-              effective_logging.log_path(log)
+              link_to 'View', show_path
             end
-
-          if log.logs_count.to_i > 0
-            link_to "View&nbsp;(#{log.logs_count}&nbsp;more)".html_safe, show_path
-          else
-            link_to 'View', show_path
           end
         end
 
