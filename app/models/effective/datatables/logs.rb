efb75d1a0765a6501ef67d7a039fab569c177a47
Matt <matthew@agilestyle.com>
2016-03-11 11:37:05 -0700

    Clean up datatable and extract its actions column into a partial

--- app/models/effective/datatables/logs.rb
+++ app/models/effective/datatables/logs.rb
@@ -10,47 +10,48 @@ if defined?(EffectiveDatatables)
           table_column :created_at
           table_column :id, visible: false
 
-          table_column :parent_id, visible: false
-          table_column :user, :if => proc { attributes[:user_id].blank? }
+          unless attributes[:user_id] || attributes[:user] || (attributes[:user] == false)
+            table_column :user
+          end
 
-          table_column :status, filter: { type: :select, values: EffectiveLogging.statuses }
+          unless attributes[:status] == false
+            table_column :status, filter: { type: :select, values: EffectiveLogging.statuses }
+          end
 
-          table_column :message, width: '50%', sortable: false
+          table_column :message
           table_column :logs_count, visible: false
 
           table_column :details, visible: false, sortable: false do |log|
-            log.details.delete(:email)
-            tableize_hash(log.details, th: true, sub_th: false, width: '100%')
+            tableize_hash(log.details.except(:email), th: true, sub_th: false, width: '100%')
           end
 
           table_column :updated_at, visible: false
 
-          table_column :actions, sortable: false, filter: false do |log|
-            show_path =
-              if datatables_active_admin_path?
-                admin_effective_log_path(log)
-              elsif datatables_admin_path?
-                effective_logging.admin_log_path(log)
-              else
-                effective_logging.log_path(log)
-              end
-
-            if log.logs_count.to_i > 0
-              link_to "View&nbsp;(#{log.logs_count}&nbsp;more)".html_safe, show_path
-            else
-              link_to 'View', show_path
-            end
-          end
+          actions_column partial: 'admin/logs/actions', partial_local: :log
         end
 
         # A nil attributes[:log_id] means give me all the top level log entries
         # If we set a log_id then it's for sub logs
         def collection
+          collection = Effective::Log.unscoped.where(parent_id: attributes[:log_id]).includes(:user, :associated)
+
           if attributes[:user_id].present?
-            Effective::Log.unscoped.where(:parent_id => attributes[:log_id]).where(:user_id => attributes[:user_id]).includes(:user)
-          else
-            Effective::Log.unscoped.where(:parent_id => attributes[:log_id]).includes(:user)
+            collection = collection.where(user_id: attributes[:user_id])
           end
+
+          if attributes[:user].present?
+            collection = collection.where(user: attributes[:user])
+          end
+
+          if attributes[:associated_id] && attributes[:associated_type]
+            collection = collection.where(associated_id: attributes[:associated_id], associated_type: attributes[:associated_type])
+          end
+
+          if attributes[:associated]
+            collection = collection.where(associated: attributes[:associated])
+          end
+
+          collection
         end
 
       end
