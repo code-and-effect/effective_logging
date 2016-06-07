59fffefce99d2fd990d6d8003a53103b6cebc381
Matt Riemer <matthew@agilestyle.com>
2016-06-07 10:22:15 -0600

    responds_to -> respond_to

--- lib/effective_logging/log_changes_user.rb
+++ lib/effective_logging/log_changes_user.rb
@@ -5,7 +5,7 @@ module EffectiveLogging
     # before_action :set_log_changes_user
 
     def set_log_changes_user
-      if responds_to?(:current_user)
+      if respond_to?(:current_user)
         EffectiveLogging.log_changes_user = current_user
       else
         raise "(effective_logging) set_log_changes_user expects a current_user() method to be available"
