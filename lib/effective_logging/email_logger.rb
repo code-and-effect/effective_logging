d971890ecd81872be5cf1681a941c7471ca942e3
Matt Riemer <matthew@codeandeffect.com>
2021-01-19 13:01:54 -0700

    log empty to emails that have a cc or bcc

--- lib/effective_logging/email_logger.rb
+++ lib/effective_logging/email_logger.rb
@@ -5,7 +5,7 @@ module EffectiveLogging
       return unless message.present?
 
       # collect a Hash of arguments used to invoke EffectiveLogger.success
-      logged_fields = { from: message.from.join(','), to: message.to, subject: message.subject }
+      logged_fields = { from: message.from.join(','), to: message.to, subject: message.subject, cc: message.cc, bcc: message.bcc }
 
       # Add a log header to your mailer to pass some objects or additional things to EffectiveLogger
       # mail(to: 'admin@example.com', subject: @post.title, log: { post: @post })
@@ -18,10 +18,11 @@ module EffectiveLogging
       end
 
       body = (message.body.try(:parts) || []).find { |part| part.content_type.to_s.downcase.include?('text/html') }
+      to = Array(message.to) - [nil, '']
 
       logged_fields[:email] = "#{message.header}<hr>#{(body.presence || message.body)}"
 
-      (message.to || []).each do |to|
+      to.each do |to|
         user = (User.where(email: to).first rescue nil)
 
         logged_fields[:to] = to
@@ -30,6 +31,11 @@ module EffectiveLogging
 
         ::EffectiveLogger.email("#{message.subject} - #{message.to.join(', ')}", logged_fields)
       end
+
+      if to.blank? && (message.cc.present? || message.bcc.present?)
+        ::EffectiveLogger.email("#{message.subject} - multiple bcc senders", logged_fields)
+      end
+
     end
 
   end
