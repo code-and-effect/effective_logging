7ecf843f89a857c23b937a55616d1bdcf9d93ed4
Matt Riemer <matthew@codeandeffect.com>
2026-03-04 15:33:35 -0700

    Upgrade test suite

--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -10,37 +10,36 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 101) do
-
+ActiveRecord::Schema[8.1].define(version: 101) do
   create_table "action_text_rich_texts", force: :cascade do |t|
-    t.string "name", null: false
     t.text "body"
-    t.string "record_type", null: false
+    t.datetime "created_at", null: false
+    t.string "name", null: false
     t.integer "record_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.string "record_type", null: false
+    t.datetime "updated_at", null: false
     t.index ["record_type", "record_id", "name"], name: "index_action_text_rich_texts_uniqueness", unique: true
   end
 
   create_table "active_storage_attachments", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "record_type", null: false
-    t.integer "record_id", null: false
     t.integer "blob_id", null: false
-    t.datetime "created_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.string "name", null: false
+    t.integer "record_id", null: false
+    t.string "record_type", null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
   end
 
   create_table "active_storage_blobs", force: :cascade do |t|
-    t.string "key", null: false
-    t.string "filename", null: false
-    t.string "content_type"
-    t.text "metadata"
-    t.string "service_name", null: false
     t.bigint "byte_size", null: false
     t.string "checksum", null: false
-    t.datetime "created_at", null: false
+    t.string "content_type"
+    t.datetime "created_at", precision: nil, null: false
+    t.string "filename", null: false
+    t.string "key", null: false
+    t.text "metadata"
+    t.string "service_name", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
@@ -51,18 +50,18 @@ ActiveRecord::Schema.define(version: 101) do
   end
 
   create_table "logs", force: :cascade do |t|
-    t.string "status"
-    t.string "user_type"
-    t.integer "user_id"
-    t.string "changes_to_type"
-    t.integer "changes_to_id"
-    t.string "associated_type"
     t.integer "associated_id"
     t.string "associated_to_s"
-    t.text "message"
+    t.string "associated_type"
+    t.integer "changes_to_id"
+    t.string "changes_to_type"
+    t.datetime "created_at", null: false
     t.text "details"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.text "message"
+    t.string "status"
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.string "user_type"
     t.index ["associated_to_s"], name: "index_logs_on_associated_to_s"
     t.index ["associated_type", "associated_id"], name: "index_logs_on_associated_type_and_associated_id"
     t.index ["changes_to_type", "changes_to_id"], name: "index_logs_on_changes_to_type_and_changes_to_id"
@@ -74,25 +73,39 @@ ActiveRecord::Schema.define(version: 101) do
     t.index ["user_id"], name: "index_logs_on_user_id"
   end
 
+  create_table "tracks", force: :cascade do |t|
+    t.string "action"
+    t.datetime "created_at", null: false
+    t.text "details"
+    t.integer "owner_id"
+    t.string "owner_type"
+    t.string "title"
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.string "user_type"
+    t.index ["owner_id"], name: "index_tracks_on_owner_id"
+    t.index ["user_id"], name: "index_tracks_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer "sign_in_count", default: 0, null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
+    t.datetime "confirmation_sent_at", precision: nil
+    t.datetime "confirmed_at", precision: nil
+    t.datetime "created_at", null: false
+    t.datetime "current_sign_in_at", precision: nil
     t.string "current_sign_in_ip"
-    t.string "last_sign_in_ip"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string "unconfirmed_email"
     t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
     t.string "first_name"
     t.string "last_name"
+    t.datetime "last_sign_in_at", precision: nil
+    t.string "last_sign_in_ip"
+    t.datetime "remember_created_at", precision: nil
+    t.datetime "reset_password_sent_at", precision: nil
+    t.string "reset_password_token"
     t.integer "roles_mask"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.integer "sign_in_count", default: 0, null: false
+    t.string "unconfirmed_email"
+    t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
